fd82e08a59cf0783d44b480f58cc51de80cc01b4
Kyle Jennings <kjennings.dev@gmail.com>
2012-04-09 20:40:52 -0500

    I may have forgot to add the knockout file lol

--- blog.py
+++ blog.py
@@ -2,35 +2,31 @@ import tornado.ioloop
 import tornado.web
 from pymongo import * 
 import simplejson as json
-	
+
 class Blog(tornado.web.RequestHandler):		
 	def get(self):
-		try:
-			# Db stuff
-			connection = Connection()
-			db = connection.Blog
-			posts = db.Posts
-			# Get the posts
-			myPosts = posts.find()
-			
-			# List
-			results = []
-			
-			# Construct a list that will be
-			# converted into JSON format
-			for post in myPosts:
-				print post['message']
-				results.append({"message" : post['message']})
-				
-			#print the results
-			print json.JSONEncoder().encode(results)
-			# send the results out in json format
-			self.set_header("Content-Type", "application/json")
-			self.set_header('Access-Control-Allow-Origin', '*')
-			self.write(json.JSONEncoder().encode(results))
-		except:
-			print 'error'
-			
+		# Db stuff
+		connection = Connection()
+        # Query the db
+		db = connection.Blog
+		posts = db.Posts
+		# Get the posts
+		myPosts = posts.find()
+		# List
+		results = []
+		# Construct a list that will be
+		# converted into JSON format
+		for post in myPosts:
+			print post['message']
+			results.append({"message" : post['message']})
+
+		# print the results
+		print json.JSONEncoder().encode(results)
+		# send the results out in json format
+		self.set_header("Content-Type", "application/json")
+		self.set_header('Access-Control-Allow-Origin', '*')
+		self.write(json.JSONEncoder().encode(results))
+
 	def post(self):
 		# Read in the request data
 		data = tornado.escape.json_decode(self.request.body)
@@ -54,7 +50,7 @@ app_settings = {
 
 # URL Routing
 application = tornado.web.Application([
-	(r"/blog", Blog)
+									(r"/blog", Blog)
 ], app_settings)
 
 # server constructor
